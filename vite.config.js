0a556f969000a94a456607ac6a39d940293deaf8
hendrism <hendrism@gmail.com>
2025-07-06 20:06:11 -0400

    fixing tailwind issues

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/card-dungeon/',
   plugins: [react()],
+  esbuild: {
+    jsx: 'automatic',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 });
